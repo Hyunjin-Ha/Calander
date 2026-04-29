9ab5175d63447b947c34d2025bcd1f0cb828e924
하현진 <prorran@gmail.com>
2026-04-29 14:19:47 +0900

    Supabase anon 키 수정

--- crossfit-calendar/src/supabase.js
+++ crossfit-calendar/src/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://sbyqjasejgmpkszmutlm.supabase.co',
-  'sb_publishable_RezVRhHbMTCKiVpDHWZUvg_X0FJwGz3'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNieXFqYXNlamdtcGtzem11dGxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0MTI2MzEsImV4cCI6MjA5Mjk4ODYzMX0.TBCrQVeAOGAOWP6jOwsDYBK04qH2e1WdyGfEcRlU25k'
 )
